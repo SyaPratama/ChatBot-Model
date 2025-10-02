abee2636ebf640838a8d8a792eef5a5202e68426
SyaPratama <rasyaputraq17@gmail.com>
2025-10-02 12:44:32 +0700

    feat(api): chatting api prototyping

---
+++ src/api/chat/conversation.ts
@@ -0,0 +1,19 @@
+import OpenAI from "openai"
+import process from "process";
+
+export async function POST(req: Request){
+    const openai = new OpenAI({
+        baseURL: "https://openrouter.ai/api/v1",
+        apiKey: process.env.OPENROUTER_API_KEY
+    })
+
+    console.debug(req,{ depth: null });
+
+    const completion = await openai.chat.completions.create({
+        model: "openai/gpt-oss-120b:free",
+        messages: [
+            
+        ]
+    })
+    return;
+}
